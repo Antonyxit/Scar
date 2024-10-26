603115a514e23957d0facf1ea460c95c06f45759
Antonyxit <carlosmorga45@gmail.com>
2024-10-26 17:11:14 -0600

    Update menu-labiblia.js

--- plugins/menu-labiblia.js
+++ plugins/menu-labiblia.js
@@ -20,50 +20,40 @@ if (!db.data.chats[m.chat].modohorny && m.isGroup) throw `*[❗] Los comandos +1
     const doc = ['pdf', 'zip', 'vnd.openxmlformats-officedocument.presentationml.presentation', 'vnd.openxmlformats-officedocument.spreadsheetml.sheet', 'vnd.openxmlformats-officedocument.wordprocessingml.document'];
     const document = doc[Math.floor(Math.random() * doc.length)];
     const str = `
- ╭━     ❄ 𝑺𝒉𝒂𝒅𝒐𝒘𝒔 𝑩𝒐𝒕 ❄
- ┃➤
- ┃➤    𝗛ola, ${taguser}
- ┃➤
- ┃➤ 𝐂𝐫𝐞𝐚𝐝𝐨𝐫 : 𝑺𝒉𝒂𝒅𝒐𝒘 ❄
- ┃➤ 𝐈𝐧𝐬𝐭𝐚𝐠𝐫𝐚𝐦 : https://www.instagram.com/shadow.vz/
- ┃➤ Nᴜᴍᴇʀᴏ wa.me/525541081250
- ┃➤ Fᴇᴄʜᴀ : ${date}
- ╰━━━━━━━━━━━━━
-
- ╭━      ❄ 𝑺𝒉𝒂𝒅𝒐𝒘𝒔 𝑩𝒐𝒕 +𝟏𝟖 ❄ 
- ┃➤ _${usedPrefix}pack_
- ┃➤ _${usedPrefix}pack2_
- ┃➤ _${usedPrefix}pack3_
- ┃➤ _${usedPrefix}videoxxx_
- ┃➤ _${usedPrefix}videolesbixxx_
- ┃➤ _${usedPrefix}tetas_
- ┃➤ _${usedPrefix}booty_ 
- ┃➤ _${usedPrefix}ecchi_
- ┃➤ _${usedPrefix}furro_
- ┃➤ _${usedPrefix}imagenlesbians_
- ┃➤ _${usedPrefix}panties_
- ┃➤ _${usedPrefix}pene_
- ┃➤ _${usedPrefix}porno_
- ┃➤ _${usedPrefix}randomxxx_
- ┃➤ _${usedPrefix}pechos_
- ┃➤ _${usedPrefix}yaoi_
- ┃➤ _${usedPrefix}yaoi2_
- ┃➤ _${usedPrefix}yuri_
- ┃➤ _${usedPrefix}yuri2_
- ┃➤ _${usedPrefix}trapito_
- ┃➤ _${usedPrefix}hentai_
- ┃➤ _${usedPrefix}nsfwloli_
- ┃➤ _${usedPrefix}nsfworgy_
- ┃➤ _${usedPrefix}nsfwfoot_
- ┃➤ _${usedPrefix}nsfwass_
- ┃➤ _${usedPrefix}nsfwbdsm_
- ┃➤ _${usedPrefix}nsfwcum_
- ┃➤ _${usedPrefix}nsfwero_
- ┃➤ _${usedPrefix}nsfwfemdom_
- ┃➤ _${usedPrefix}nsfwglass_
- ┃➤ _${usedPrefix}hentaipdf *<texto>*_
- ┃➤ _${usedPrefix}hentaisearch *<texto>*_
- ╰━━━━━━━━━ 𝑺𝒉𝒂𝒅𝒐𝒘𝒔 𝑩𝒐𝒕 ━━━━`.trim();
+┃≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡┃
+┣ ඬ⃟ 🔞 _${usedPrefix}pack_
+┣ ඬ⃟ 🔞 _${usedPrefix}pack2_
+┣ ඬ⃟ 🔞 _${usedPrefix}pack3_
+┣ ඬ⃟ 🔞 _${usedPrefix}videoxxx_
+┣ ඬ⃟ 🔞 _${usedPrefix}videolesbixxx_
+┣ ඬ⃟ 🔞 _${usedPrefix}tetas_
+┣ ඬ⃟ 🔞 _${usedPrefix}booty_
+┣ ඬ⃟ 🔞 _${usedPrefix}ecchi_
+┣ ඬ⃟ 🔞 _${usedPrefix}furro_
+┣ ඬ⃟ 🔞 _${usedPrefix}imagenlesbians_
+┣ ඬ⃟ 🔞 _${usedPrefix}panties_
+┣ ඬ⃟ 🔞 _${usedPrefix}pene_
+┣ ඬ⃟ 🔞 _${usedPrefix}porno_
+┣ ඬ⃟ 🔞 _${usedPrefix}randomxxx_
+┣ ඬ⃟ 🔞 _${usedPrefix}pechos_
+┣ ඬ⃟ 🔞 _${usedPrefix}yaoi_
+┣ ඬ⃟ 🔞 _${usedPrefix}yaoi2_
+┣ ඬ⃟ 🔞 _${usedPrefix}yuri_
+┣ ඬ⃟ 🔞 _${usedPrefix}yuri2_
+┣ ඬ⃟ 🔞 _${usedPrefix}trapito_
+┣ ඬ⃟ 🔞 _${usedPrefix}hentai_
+┣ ඬ⃟ 🔞 _${usedPrefix}nsfwloli_
+┣ ඬ⃟ 🔞 _${usedPrefix}nsfworgy_
+┣ ඬ⃟ 🔞 _${usedPrefix}nsfwfoot_
+┣ ඬ⃟ 🔞 _${usedPrefix}nsfwass_
+┣ ඬ⃟ 🔞 _${usedPrefix}nsfwbdsm_
+┣ ඬ⃟ 🔞 _${usedPrefix}nsfwcum_
+┣ ඬ⃟ 🔞 _${usedPrefix}nsfwero_
+┣ ඬ⃟ 🔞 _${usedPrefix}nsfwfemdom_
+┣ ඬ⃟ 🔞 _${usedPrefix}nsfwglass_
+┣ ඬ⃟ 🔞 _${usedPrefix}hentaipdf *<texto>*_
+┣ ඬ⃟ 🔞 _${usedPrefix}hentaisearch *<texto>*_
+┗━━━━━━━━━━━━━━━━┛`.trim();
     if (m.isGroup) {
       await conn.sendMessage(m.chat, {image: pp, caption: str.trim(), mentions: [...str.matchAll(/@([0-9]{5,16}|0)/g)].map((v) => v[1] + '@s.whatsapp.net')}, {quoted: m});
       await conn.sendFile(m.chat, vn, 'La biblia.mp3', null, m, true, {type: 'audioMessage', ptt: true});
