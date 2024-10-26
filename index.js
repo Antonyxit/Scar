4636aa9da15dad03c18036462dea3251324d4529
Antonyxit <carlosmorga45@gmail.com>
2024-10-26 16:39:17 -0600

    Update index.js

--- index.js
+++ index.js
@@ -13,11 +13,11 @@ const {name, author} = require(join(__dirname, './package.json'));
 const {say} = cfonts;
 const rl = createInterface(process.stdin, process.stdout);
 
-say('Shadows Bot\nWhatsApp Bot', {
+say('Bot\nWhatsApp', {
   font: 'chrome',
   align: 'center',
   gradient: ['red', 'magenta']});
-say(`Bot Created By Shadow`, {
+say(`Bot Created By AntonyxT`, {
   font: 'console',
   align: 'center',
   gradient: ['red', 'magenta']});
